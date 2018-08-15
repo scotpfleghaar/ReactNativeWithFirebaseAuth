a7267fe0775c17b5bef4382ba7ff2a00e90d135b
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-15 12:16:25 -0500

    Two Way binding state

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -3,11 +3,18 @@ import {View, StyleSheet, TextInput} from 'react-native';
 import {Button, Card, CardSection} from "./Common";
 
 class LoginForm extends Component {
+    state = {
+        text: ''
+    }
+
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput style={{height: 20, width: 100}}/>
+                    <TextInput
+                        value={this.state.text}
+                        onChangeText={text => this.setState({text})}
+                        style={{height: 20, width: 100}}/>
                 </CardSection>
 
                 <CardSection>
