ad4dd2552502be1dc93d63129d363719517b22e5
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-15 10:40:02 -0500

    Imported and connected the Firebase App to React Native

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import firebase from 'firebase';
 import {StyleSheet, Text, View} from 'react-native';
 import {Header} from './src/Components/Common';
 
 
 type Props = {};
 export default class App extends Component<Props> {
+    componentWillMount() {
+        // Initialize Firebase
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAaeRLwgdt5dy9TV2RIzF4xTr402fJ-94c',
+            authDomain: "reactauthenticationapp.firebaseapp.com",
+            databaseURL: "https://reactauthenticationapp.firebaseio.com",
+            projectId: "reactauthenticationapp",
+            storageBucket: "reactauthenticationapp.appspot.com",
+            messagingSenderId: "908900560010"
+        });
+    }
+
     render() {
         return (
             <View>
