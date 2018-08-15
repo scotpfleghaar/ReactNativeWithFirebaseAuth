8839941ca0fb2f352928cc787896f9eaf1989e10
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-15 14:38:15 -0500

    Implemented onAuthStateChange function to determine if the user is signed in.

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ import LoginForm from './src/Components/LoginForm';
 
 type Props = {};
 export default class App extends Component<Props> {
+    state = {
+        isLoggedIn: false
+    };
+
     componentWillMount() {
         // Initialize Firebase
         firebase.initializeApp({
@@ -17,6 +21,15 @@ export default class App extends Component<Props> {
             storageBucket: "reactauthenticationapp.appspot.com",
             messagingSenderId: "908900560010"
         });
+
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({isLoggedIn: true})
+            } else {
+                this.setState({isLoggedIn: false})
+            }
+        })
     }
 
     render() {
