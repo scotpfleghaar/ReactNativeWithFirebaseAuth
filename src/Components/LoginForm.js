48f424c1e0d8d6ada5a3a9cc730f4baa6cc4bcce
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-15 12:10:58 -0500

    Using the React TextInput Built in Component

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 import {Button, Card, CardSection} from "./Common";
 
 class LoginForm extends Component {
     render() {
         return (
             <Card>
-                <CardSection/>
-                <CardSection/>
+                <CardSection>
+                    <TextInput style={{height: 20, width: 100}}/>
+                </CardSection>
+
+                <CardSection>
+                    <TextInput style={{height: 20, width: 100}}/>
+                </CardSection>
+
                 <CardSection>
                     <Button>
                         Log In/Sign Up
